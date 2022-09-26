e04a3c93eead9a7909d2dc813bab371e7a6d6d24
ErwinPrograms <erwinprograms@gmail.com>
2022-09-26 13:59:47 -0700

    Add model POJOs

---
+++ src/model/Contact.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Contact {
+
+    private int contactID;
+    private String contactName;
+    private String email;
+
+    public Contact(int contactID, String contactName, String email) {
+        this.contactID = contactID;
+        this.contactName = contactName;
+        this.email = email;
+    }
+
+    public int getContactID() {
+        return contactID;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ src/model/FirstLevelDivision.java
@@ -0,0 +1,26 @@
+package model;
+
+public class FirstLevelDivision {
+
+    private int divisionID;
+    private String division;
+    private int countryID;
+
+    public FirstLevelDivision(int divisionID, String division, int countryID) {
+        this.divisionID = divisionID;
+        this.division = division;
+        this.countryID = countryID;
+    }
+
+    public int getDivisionID() {
+        return divisionID;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public int getCountryID() {
+        return countryID;
+    }
+}
