c3d11ec799791ef132a1ada918830278a0499290
ErwinPrograms <erwinprograms@gmail.com>
2022-10-08 18:55:15 -0700

    Add form change on login

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,13 +7,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import main.dao.DBConnection;
 
+import java.util.Objects;
+
 import static javafx.application.Application.launch;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/main/view/LoginForm.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(
+                getClass().getResource("/main/view/LoginForm.fxml")
+        ));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
--- src/main/controller/LoginFormController.java
+++ src/main/controller/LoginFormController.java
@@ -1,16 +1,23 @@
 package main.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import main.dao.DBConnection;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import main.dao.UserDao;
 import main.model.User;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.time.ZoneId;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LoginFormController implements Initializable {
@@ -21,6 +28,9 @@ public class LoginFormController implements Initializable {
     PasswordField passwordField;
     @FXML
     Label zoneLabel;
+
+    @FXML
+    AnchorPane loginFormParent;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -35,11 +45,26 @@ public class LoginFormController implements Initializable {
         UserDao userDao = new UserDao();
         User dbUserMatch = userDao.getByUsername(inputUsername);
         if (dbUserMatch == null || !dbUserMatch.getPassword().equals(inputPassword)) {
-            //TODO: alert failure
-            System.out.println("FAILURE");
+            JOptionPane.showMessageDialog(null,
+                    "Failed to login with those credentials.");
         } else {
             //TODO: success
-            System.out.println("Success");
+            //TODO: track user activity
+            //TODO: alert when appointment within 15 minutes of login or custom message
+            changeToCustomerForm();
         }
     }
+
+    public void changeToCustomerForm(){
+        try {
+            Parent nextForm = FXMLLoader.load(Objects.requireNonNull(
+                    getClass().getResource("/main/view/CustomerForm.fxml")
+            ));
+            Stage applicationStage = (Stage) loginFormParent.getScene().getWindow();
+            applicationStage.setScene(new Scene(nextForm, 800, 600));
+        } catch (IOException ex) {
+            System.out.println("Customer from could not be loaded: " + ex.getMessage());
+        }
+    }
+
 }
