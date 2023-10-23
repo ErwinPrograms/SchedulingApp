4c17f55b4830d5b8d4d69ac1be1e9e8f05a70fcb
ErwinPrograms <erwinprograms@gmail.com>
2023-10-22 17:36:10 -0700

    Add Javadoc comments to DataHandlingFacade

--- src/main/utility/DataHandlingFacade.java
+++ src/main/utility/DataHandlingFacade.java
@@ -23,6 +23,9 @@ public class DataHandlingFacade {
     private ArrayList<Customer> customers = new ArrayList<>();
     private ArrayList<Appointment> appointments = new ArrayList<>();
 
+    /**
+     * Initializes DataHandlingFacade by fetching data from multiple tables in the database
+     */
     public DataHandlingFacade() {
         fetchInitialData();
         refreshCustomerData();
@@ -84,14 +87,30 @@ public class DataHandlingFacade {
         appointments = new AppointmentDao().getAll();
     }
 
+    /**
+     * Creates an ObservableList of all customers in the database using customers ArrayList that's already stored
+     * @return ObservableList containing all customers from database
+     */
     public ObservableList<Customer> customersObservableList() {
         return FXCollections.observableList(customers);
     }
 
+    /**
+     * Creates an ObservableList of all appointments in the database using appointments Arraylist that's already stored
+     * @return ObservableList containing all appointments from database
+     */
     public ObservableList<Appointment> appointmentsObservableList() {
         return FXCollections.observableList(appointments);
     }
 
+    /**
+     * Finds all appointments which are between the two LocalDateTimes. This includes appointments which are only partially
+     * inside the time range
+     * @param startOfTimeRange  the starting time to check
+     * @param endOfTimeRange    the ending time to check
+     * @return ObservableList containing all appointments in database that are between the two time ranges. If there
+     * are none, then an empty ObservableList is returned.
+     */
     public ObservableList<Appointment> appointmentsObservableList(LocalDateTime startOfTimeRange,
                                                                   LocalDateTime endOfTimeRange) {
         ObservableList<Appointment> result = FXCollections.observableArrayList();
@@ -107,10 +126,19 @@ public class DataHandlingFacade {
         return result;
     }
 
+    /**
+     * Gives an ObservableList of division names which are associated with the parameter countryName
+     * @param countryName   Name of country to get divisions for
+     * @return  ObservableList of division names which are in the country
+     */
     public ObservableList<String> divisionNamesObservableList(String countryName) {
         return countryDivisionNamesMap.get(countryByName(countryName));
     }
 
+    /**
+     * Creates and returns an ObservableList of all country names inside the database
+     * @return  ObservableList of country names
+     */
     public ObservableList<String> countriesObservableList() {
         ObservableList<String> countryNames = FXCollections.observableArrayList();
         countries.forEach(countryObj -> countryNames.add(countryObj.getCountry()));
@@ -118,10 +146,20 @@ public class DataHandlingFacade {
         return countryNames;
     }
 
+    /**
+     * Creates an ObservableList of all contacts in the database using contacts Arraylist that's already stored
+     * @return  ObservableList of all contacts in database
+     */
     public ObservableList<Contact> contactObservableList() {
         return FXCollections.observableList(contacts);
     }
 
+    /**
+     * Searches for a division which matches the input ID, then returns the result. Looks through stored divisions
+     * ArrayList instead of making a new database call.
+     * @param divisionID    ID of division to search for
+     * @return  null if there isn't exactly one unique match; if there is, then the FirstLevelDivision match
+     */
     public FirstLevelDivision divisionByID(int divisionID) {
         ArrayList<FirstLevelDivision> matchedDivisions = new ArrayList<>();
         divisions.forEach(division -> {
@@ -138,12 +176,10 @@ public class DataHandlingFacade {
     }
 
     /**
-     *
      * @param divisionName  Required parameter. Finds a stored division with the same name.
-     * @param countryName   Optional parameter. Allows for further checking by a countryName string
-     * @return
+     * @return  FirstLevelDivision that matches divisionName.
      */
-    public FirstLevelDivision divisionByStrings(String divisionName, String... countryName) {
+    public FirstLevelDivision divisionByStrings(String divisionName) {
         ArrayList<FirstLevelDivision> matchedDivisions = new ArrayList<>();
         divisions.forEach(division -> {
             if(division.getDivision().equals(divisionName))
@@ -159,6 +195,12 @@ public class DataHandlingFacade {
         return null;
     }
 
+    /**
+     * Finds a country based on an input countryID. Looks through stored countries ArrayList instead of making a new
+     * database call.
+     * @param countryID     countryID to search for
+     * @return  Country object that matches countryID. Null if no matches are found
+     */
     public Country countryByID(int countryID) {
         ArrayList<Country> matchedCountries = new ArrayList<>();
         countries.forEach(country -> {
@@ -174,6 +216,12 @@ public class DataHandlingFacade {
         return matchedCountries.get(0);
     }
 
+    /**
+     * Finds a country based on an input countryName. Looks through stored countries ArrayList instead of making a new
+     * database call.
+     * @param countryName   name of country to look for
+     * @return  Country object with matching name. Null if no matches are found.
+     */
     public Country countryByName(String countryName) {
         ArrayList<Country> matchedCountries = new ArrayList<>();
         countries.forEach(country -> {
@@ -189,16 +237,28 @@ public class DataHandlingFacade {
         return matchedCountries.get(0);
     }
 
+    /**
+     * Finds country based on a FirstLevelDivision input.
+     * @param division  FirstLevelDivision to find corresponding country for
+     * @return  Country object that matches division
+     */
     public Country countryByDivision(FirstLevelDivision division) {
         return divisionCountryMap.get(division);
     }
 
     //TODO: lookup contact using private member variable contacts so less database calls are made
+
+    /**
+     * Gets the contact based on input contactID
+     * @param contactID     id of the contact
+     * @return  A contact object that matches the ID.
+     */
     public Contact contactByID(int contactID) {
+        //TODO: get contact by searching through contacts ArrayList instead of calling database
         return new ContactDao().getByID(contactID);
     }
 
-    public int customerMaxID() {
+    private int customerMaxID() {
         int maxID = 0;
 
         for (Customer customer: customers) {
@@ -211,6 +271,18 @@ public class DataHandlingFacade {
     }
 
     //TODO: accept User/UserName as a parameter to be relayed to DAO
+
+    /**
+     * Creates a Customer object then accesses CustomerDao to insert into database
+     * @param name          name of customer
+     * @param address       address of customer
+     * @param postalCode    postal code of customer
+     * @param phone         phone number of customer
+     * @param divisionName  division name of customer
+     * @param countryName   country name of customer
+     * @param loggedInUser  the logged-in user that created this customer
+     * @return  Status code: 0 - successful, otherwise unsuccessful
+     */
     public int insertCustomer(String name, String address, String postalCode,
                               String phone, String divisionName, String countryName,
                               User loggedInUser) {
@@ -233,6 +305,16 @@ public class DataHandlingFacade {
         return status;
     }
 
+    /**
+     * Creates a Customer object then accesses CustomerDao to update existing customer in database
+     * @param name          name of customer
+     * @param address       address of customer
+     * @param postalCode    postal code of customer
+     * @param phone         phone number of customer
+     * @param divisionName  division name of customer
+     * @param countryName   country name of customer
+     * @return  Status code: 0 - successful, otherwise unsuccessful
+     */
     public int updateCustomer(int customerID, String name, String address, String postalCode,
                               String phone, String divisionName, String countryName) {
         int divisionID = divisionByStrings(divisionName).getDivisionID();
@@ -254,6 +336,11 @@ public class DataHandlingFacade {
         return status;
     }
 
+    /**
+     * Deletes a customer from database
+     * @param id    id of customer to delete
+     * @return  Status code: 0 - successful, otherwise failure
+     */
     public int deleteCustomer(int id){
         ArrayList<Appointment> appointmentsToDelete = appointmentsByCustomerID(id);
 
@@ -271,6 +358,12 @@ public class DataHandlingFacade {
         return status;
     }
 
+    /**
+     * Gets all appointments that match a customerID
+     * @param customerID    customerID that all match appointments should have
+     * @return  ArrayList of all appointments which contain the customerID. If there are none then an empty ArrayList
+     * is returned
+     */
     public ArrayList<Appointment> appointmentsByCustomerID(int customerID) {
         ArrayList<Appointment> customerAppointments = new ArrayList<>();
 
@@ -283,6 +376,13 @@ public class DataHandlingFacade {
         return customerAppointments;
     }
 
+    //TODO: test to see if this works when time zone is affected
+    /**
+     * Finds any appointments that are occurring within the input amount of minutes
+     * @param minutesUntilStart     The amount of minutes in the future to look for
+     * @return  An ArrayList of all appointments that are occurring within minutesUntilStarts minutes. An empty
+     * ArrayList is returned when there are no matches.
+     */
     public ArrayList<Appointment> upcomingAppointments(int minutesUntilStart) {
         LocalDateTime currentTime = LocalDateTime.now();
         LocalDateTime latestToCheck = currentTime.plusMinutes(minutesUntilStart);
@@ -299,7 +399,7 @@ public class DataHandlingFacade {
     }
 
     //TODO: refactor this so that DAO takes care of getting maxID
-    public int appointmentMaxID() {
+    private int appointmentMaxID() {
         int maxID = 0;
 
         for (Appointment appointment: appointments) {
@@ -310,6 +410,20 @@ public class DataHandlingFacade {
 
         return maxID;
     }
+
+    /**
+     * Creates an Appointment object, then attempts to insert it into the database using AppointmentDao.
+     * @param title         tile of appointment
+     * @param description   description of appointment
+     * @param location      location of appointment
+     * @param type          type of appointment
+     * @param start         start of appointment
+     * @param end           end of appointment
+     * @param customerID    customerID of appointment
+     * @param userID        userID of appointment
+     * @param contactID     contactID of appointment
+     * @return  Status code: 0 - if successful, otherwise a failure
+     */
     public int insertAppointment(String title, String description,
                                  String location, String type,
                                  LocalDateTime start, LocalDateTime end,
@@ -338,6 +452,19 @@ public class DataHandlingFacade {
         return status;
     }
 
+    /**
+     * Creates an Appointment object, then attempts to update it into the database using AppointmentDao.
+     * @param title         tile of appointment
+     * @param description   description of appointment
+     * @param location      location of appointment
+     * @param type          type of appointment
+     * @param start         start of appointment
+     * @param end           end of appointment
+     * @param customerID    customerID of appointment
+     * @param userID        userID of appointment
+     * @param contactID     contactID of appointment
+     * @return  Status code: 0 - if successful, otherwise a failure
+     */
     public int updateAppointment(int appointmentID,
                                  String title,
                                  String description,
@@ -370,6 +497,11 @@ public class DataHandlingFacade {
         return status;
     }
 
+    /**
+     * Deletes an appointment from the database using an id
+     * @param id    id of appointment to delete
+     * @return  Status code: 0 - if successful, otherwise a failure
+     */
     public int deleteAppointment(int id) {
         int status = new AppointmentDao().delete(id);
 
@@ -379,6 +511,12 @@ public class DataHandlingFacade {
         return status;
     }
 
+    /**
+     * Gets all appointments associated with the input Contact object and combines them into an ObservableList
+     * @param contact   Contact to find matches for
+     * @return  An ObservableList that contains all matching Appointments. If there are no matches, then this
+     * ObservableList will be empty.
+     */
     public ObservableList<Appointment> appointmentsByContact(Contact contact) {
         ObservableList<Appointment> contactAppointments = FXCollections.observableArrayList();
 
@@ -391,12 +529,20 @@ public class DataHandlingFacade {
         return contactAppointments;
     }
 
+    /**
+     * Calls ReportDao to retrieve an ArrayList of all MonthTypeCounts, then converts it into an ObservableList
+     * @return  ObservableList of all MonthTypeCount aggregate items
+     */
     public ObservableList<MonthTypeCount> getMonthTypeCountReport() {
         return FXCollections.observableList(
                 new ReportDao().getMonthTypeCountReport()
         );
     }
 
+    /**
+     * Calls ReportDao to retrieve an ArrayList of all DivisionCounts, then converts it into an ObservableList
+     * @return  ObservableList of all DivisionCount aggregate items
+     */
     public ObservableList<DivisionCount> getDivisionCountReport() {
         //Potential to implement caching so database isn't excessively called
         return FXCollections.observableList(
